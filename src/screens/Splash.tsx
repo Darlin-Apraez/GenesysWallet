c767a953e24fa834f0ace26237f4fb89759d62a0
DarlinCondC <darlinapraez@condorcoin.co>
2022-03-25 13:38:20 -0500

    Splash screen

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -5,58 +5,34 @@ import {
   stylesO,
   stylesS,
 } from "./../appTheme/styles/styles";
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, SafeAreaView, Image } from "react-native";
 import BarStatus from "../components/BarStatus";
-import React, { useState } from "react";
+import React from "react";
 import * as Animatable from "react-native-animatable";
 
 const Splash = () => {
-  //Modales
-  // const [anmt, setanmt] = useState("");
-
-  // setanmt("fadeIn"); animation={anmt} duration={600}
-
-  const fadeIn = {
-    from: {
-      opacity: 0,
-    },
-    to: {
-      opacity: 1,
-    },
-  };
-
-  const zoomIn = {
-    0: {
-      opacity: 0,
-      scale: 0,
-    },
-    0.5: {
-      opacity: 1,
-      scale: 0.3,
-    },
-    1: {
-      opacity: 1,
-      scale: 1,
-    },
-  };
-
   return (
     <SafeAreaView style={stylesB.body}>
       <BarStatus />
       <View style={stylesB.completo}>
-        {/* <Text style={[stylesM.textColorWhite, stylesM.fontSizeSixteen]}>
-          Splash
-        </Text> */}
         <Animatable.View
           animation="pulse"
           easing="ease-out"
           iterationCount="infinite"
         >
           <Image
-            style={stylesM.logoGenesys}
+            style={stylesM.completo__logoGenesys}
             source={require("./../img/GENESYS-LOGO.png")}
           />
+          <Image
+            style={stylesM.completo__radiance}
+            source={require("./../img/radianceSplash.png")}
+          />
         </Animatable.View>
+        <Image
+          style={stylesM.completo__lettersVortex}
+          source={require("./../img/VORTEX.png")}
+        />
       </View>
     </SafeAreaView>
   );
